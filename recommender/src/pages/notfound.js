800ae9cf4a29d5393ba6d84a45aaf230ad229297
YE-EUN <musiclove529@naver.com>
2022-05-31 06:11:20 +0900

    notfound

---
+++ recommender/src/pages/notfound.js
@@ -0,0 +1,12 @@
+import { Container } from "react-bootstrap";
+
+function NotFound(){
+
+    return(
+        <Container className="recommend mt-5">
+            404
+        </Container>
+    )
+
+}
+export default NotFound;
--- recommender/src/pages/page.js
+++ recommender/src/pages/page.js
@@ -2,6 +2,7 @@ import { Container } from "react-bootstrap";
 import {Routes, Route} from 'react-router-dom';
 import Header from "../components/header";
 import Home from "./home";
+import NotFound from "./notfound";
 import Recommend from "./recommend";
 
 function Page(){
@@ -11,6 +12,7 @@ function Page(){
             <Routes>
                 <Route path="/" element={<Home></Home>} exact></Route>
                 <Route path="/recommend" element={<Recommend></Recommend>}></Route>
+                <Route path="*" element={<NotFound></NotFound>}></Route>
             </Routes>
         </Container>
     )

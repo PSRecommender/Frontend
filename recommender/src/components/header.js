3d2c3e689e0dd5ba06ad8e61c880e9357a89916f
YE-EUN <musiclove529@naver.com>
2022-06-04 18:52:42 +0900

    favicon

--- recommender/src/components/header.js
+++ recommender/src/components/header.js
@@ -1,8 +1,13 @@
+import { Navbar } from "react-bootstrap";
+
 function Header(){
     return(
-        <nav class="navbar  static-top">
-            <div class="container">
-                <a class="navbar-brand" href="/"><h3>어쩔코테</h3></a>
+        <nav className="navbar static-top mb-3">
+            <div className="container">
+                <Navbar.Brand href="/">
+                    <img src='../../favicon.png' width={35} height={35} className="me-2 d-inline-block align-top"></img>
+                    <h3 className="d-inline-block align-top">어쩔코테</h3>
+                </Navbar.Brand>   
             </div>
         </nav>
     )
--- recommender/src/pages/recommend.js
+++ recommender/src/pages/recommend.js
@@ -5,10 +5,12 @@ import axios from "axios";
 
 function Recommend(){
     const [data, setData] = useState(null);
-    const [error, setError] = useState(false)
+    const [error, setError] = useState(false);
+
+    const userId = window.location.href.split("?")[1];
+    console.log(userId)
     
     const getData = () => {
-        let userId = window.location.href.split("?")[1];
         axios.post("/recommend", {userId:userId})
         .then(function(response){
             if(typeof(response.data.result) === "number") setError(true);
@@ -23,13 +25,25 @@ function Recommend(){
     }
     return(
         error?
-        <Container className="recommend mt-5" style={{"text-align":"center"}}>
-            <h1>{data!==-1?data:"문제를 추천할 수 없습니다."}</h1>
+        <Container className="recommend" style={{"text-align":"center"}}>
+            <h3>{data!==-1?data:"문제를 추천할 수 없습니다."}</h3>
         </Container>
         :
-        <Container className="recommend mt-5">
-            <h5 className="mb-3">추천 문제</h5>
-            <RecommendTable data={data}></RecommendTable>
+        <Container>
+            <Container className="recommend mb-4" style={{"backgroundColor":"#DCEBFF", "padding":"20px"}}>
+                <h5 className="mb-3">{userId}님의 학습레포트</h5>
+                
+            </Container>        
+
+            <Container className="recommend mb-4" style={{"backgroundColor":"#DCEBFF", "padding":"20px"}}>
+                <h5 className="mb-3">{userId}님의 학습 시퀀스 기반 추천 문제</h5>
+                <RecommendTable data={data}></RecommendTable>
+            </Container>
+            
+            <Container className="recommend" style={{"backgroundColor":"#DCEBFF", "padding":"20px"}}>
+                <h5 className="mb-3">주요 알고리즘 추천 문제</h5>
+                <RecommendTable data={data}></RecommendTable>
+            </Container>
         </Container>
     )
 
